5eff7242a075f8f17d0228238e052a2596f1601d
Abraham <cisnerosjesus622@gmail.com>
2019-10-16 12:47:36 -0500

    First Commit

---
+++ TDA/Dado.py
@@ -0,0 +1,12 @@
+import random
+
+class Dado:
+
+    def __init__(self):
+        self.valor=0
+   
+    def tirarDado(self):
+        self.valor = random.randint(1,6)
+
+    def getValor(self):
+        return self.valor
